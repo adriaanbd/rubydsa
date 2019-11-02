50411d10c7c36cc883272f771db66c4394a4ed6a
Adriaan Beiertz <adriaanbd@gmail.com>
2019-11-02 19:44:03 -0300

    base case 0

--- find_square_recursion.rb
+++ find_square_recursion.rb
@@ -1,7 +1,8 @@
 def find_square(num)
-  return num if num == 1
+  return num if num.zero?
   return find_square(num - 1) + 2 * num - 1
 end
 
 p find_square(5) == 25
 p find_square(10) == 100
+p find_square(100) == 10000
